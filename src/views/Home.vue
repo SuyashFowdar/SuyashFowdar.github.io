4046d56392d2cb79bb0a2ce4a3ad28a369e3d20a
Suyash <suyash.krishna9@gmail>
2021-06-02 11:25:59 +0400

    Convert to vue project

---
+++ src/views/Home.vue
@@ -0,0 +1,210 @@
+<template>
+  <div class="home">
+    <section id="me" class="row-lg col-sm cross-center">
+      <div class="flex h-100 col main-center">
+        <h1 class="row">Suyash Fowdar</h1>
+        <h2>Dreamer. Drama Artist. <br> Software Developer.</h2>
+      </div>
+      <div class="flex h-100 col main-center">
+        <p>4+ years of experience in  Full-Stack Software Development. </p>
+        <p>Expert in building static and dynamic Websites or Mobile Apps as per your requirements.</p>
+        <p>For more info, contact me.</p>
+        <div class="links row wrap">
+          <a href="https://www.github.com/suyashfowdar/" target="_blank"><i class="fab fa-github fa-2x"></i></a>
+          <a href="https://twitter.com/SuyashFowdar" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+          <a href="https://www.linkedin.com/in/suyash-fowdar/" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
+          <a href="mailto:suyash.krishna9@gmail.com"><i class="fas fab fa-envelope fa-2x"></i></a>
+          <a href="pdf/SuyashFowdar.pdf" class="clickable row cross-center" target="_blank">Resume</a>
+        </div>
+      </div>
+    </section>
+    <section id="skills" class="col cross-center main-center">
+      <h2>My Skills</h2>
+      <div class="row w-100 wrap main-center cross-start">
+        <div class="row main-space-around cross-start wrap">
+          <article class="col main-center cross-start">
+            <h3>Languages</h3>
+            <ul>
+              <li>JavaScript</li>
+              <li>Ruby</li>
+              <li>Dart</li>
+              <li>HTML5</li>
+              <li>CSS3</li>
+            </ul>
+          </article>
+          <article class="col main-center cross-start">
+            <h3>Frameworks & <br class="hide-sm">Libraries</h3>
+            <ul>
+              <li>VueJS</li>
+              <li>NodeJS</li>
+              <li>Flutter</li>
+              <li>Rails</li>
+              <li>Bootstrap</li>
+              <li>ExpressJS</li>
+              <li>AngularJS</li>
+              <li>NativeScript</li>
+            </ul>
+          </article>
+        </div>
+        <div class="row main-space-around cross-start wrap">
+          <article class="col main-center cross-start">
+            <h3>Soft Skills</h3>
+            <ul>
+              <li>Problem Solving</li>
+              <li>Public Speaking</li>
+              <li>Teamwork</li>
+              <li>Team Leadership</li>
+              <li>Critical Thinking</li>
+            </ul>
+          </article>
+          <article class="col main-center cross-start">
+            <h3>Speaking Languages</h3>
+            <ul>
+              <li>English</li>
+              <li>French</li>
+              <li>Hindi</li>
+              <li>Mauritian Creole</li>
+              <li>Mauritian Bhojpuri</li>
+            </ul>
+          </article>
+        </div>
+      </div>
+    </section>
+    <section id="projects" class="col w-100 main-center cross-center">
+      <h2>My Projects</h2>
+      <div class="row wrap main-space-around">
+        <article class="row w-45-lg w-90-sm shadow-15 cross-center">
+          <div class="img work-1"></div>
+          <div>
+            <h3>Vaishnava App</h3>
+            <p>Vaishnava App is a useful tool for of Sri Chaitanya Saraswat Math lovers.</p>
+            <p><strong>HTML | CSS | JS | Vue</strong></p>
+            <div class="row wrap">
+              <a href="https://www.github.com/scsmathapp/vaishnava" target="_blank" class="clickable row main-center">View Source</a>
+              <a href="https://scsmathapp.github.io/vaishnava" target="_blank" class="clickable row main-center">View Live App</a>
+            </div>
+          </div>
+        </article>
+        <article class="row w-45-lg w-90-sm shadow-15 cross-center">
+          <div class="img work-2"></div>
+          <div>
+            <h3>Krishna Shop</h3>
+            <p>An online shop project for Gaudiya Tradition lovers.</p>
+            <p><strong>HTML | CSS</strong></p>
+            <div class="row wrap">
+              <a href="https://www.github.com/suyashfowdar/Krishna_Shop" target="_blank" class="clickable row main-center">View Source</a>
+              <a href="https://suyashfowdar.github.io/Krishna_Shop" target="_blank" class="clickable row main-center">View Live App</a>
+            </div>
+          </div>
+        </article>
+        <article class="row w-45-lg w-90-sm shadow-15 cross-center">
+          <div class="img work-3"></div>
+          <div>
+            <h3>Tech News</h3>
+            <p>An article page project for tech updates.</p>
+            <p><strong>HTML | CSS | Ruby | Rails</strong></p>
+            <div class="row wrap">
+              <a href="https://www.github.com/suyashfowdar/tech-news" target="_blank" class="clickable row main-center">View Source</a>
+              <a href="https://guarded-reaches-31670.herokuapp.com/" target="_blank" class="clickable row main-center">View Live App</a>
+            </div>
+          </div>
+        </article>
+        <article class="row w-45-lg w-90-sm shadow-15 cross-center">
+          <div class="img work-4"></div>
+          <div>
+            <h3>Bike Shooter</h3>
+            <p>An action pack shooting game with bikes.</p>
+            <p><strong>HTML | CSS | JS</strong></p>
+            <div class="row wrap">
+              <a href="https://www.github.com/suyashfowdar/bike-shooter" target="_blank" class="clickable row main-center">View Source</a>
+              <a href="https://suyashfowdar.github.io/bike-shooter" target="_blank" class="clickable row main-center">View Live App</a>
+            </div>
+          </div>
+        </article>
+        <article class="row w-45-lg w-90-sm shadow-15 cross-center">
+          <div class="img work-5"></div>
+          <div>
+            <h3>Meow</h3>
+            <p>An image gallery for cat lovers.</p>
+            <p><strong>HTML | CSS | JS | React</strong></p>
+            <div class="row wrap">
+              <a href="https://www.github.com/suyashfowdar/meow" target="_blank" class="clickable row main-center">View Source</a>
+              <a href="https://suyashfowdar.github.io/meow" target="_blank" class="clickable row main-center">View Live App</a>
+            </div>
+          </div>
+        </article>
+        <article class="row w-45-lg w-90-sm shadow-15 cross-center">
+          <div class="img work-6"></div>
+          <div>
+            <h3>Baby Tracker</h3>
+            <p>Mobile web app for tracking baby measurements.</p>
+            <p><strong>HTML | CSS | JS | React | Ruby | Rails</strong></p>
+            <div class="row wrap">
+              <a href="https://www.github.com/suyashfowdar/baby-tracker-front" target="_blank" class="clickable row main-center">View Source</a>
+              <a href="https://suyashfowdar.github.io/baby-tracker-front" target="_blank" class="clickable row main-center">View Live App</a>
+            </div>
+          </div>
+        </article>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  created() {
+    let externalScript = document.createElement('script')
+    externalScript.setAttribute('src', 'https://kit.fontawesome.com/bcea3aca40.js')
+    document.head.appendChild(externalScript);
+    let skills = document.getElementById('skills'),
+      projects = document.getElementById('projects'),
+      meLink = document.getElementById('me-link'),
+      skillsLink = document.getElementById('skills-link'),
+      projectsLink = document.getElementById('projects-link'),
+      header = document.querySelector('header'),
+      skillsOffset, projectsOffset;
+
+    if (window.innerWidth <= 600) {
+      skillsOffset = skills.offsetTop - window.innerHeight + 80;
+      projectsOffset = projects.offsetTop - window.innerHeight + 80;
+    } else {
+      skillsOffset = skills.offsetTop - 40;
+      projectsOffset = projects.offsetTop - 40;
+    }
+
+    window.onscroll = function() {
+      if (window.scrollY < skillsOffset) {
+        if (header.classList.contains('even-color')) {
+          header.classList.remove('even-color');
+          meLink.classList.add('current-link');
+          skillsLink.classList.remove('current-link');
+        }
+      } else if (window.scrollY < projectsOffset) {
+        if (!header.classList.contains('even-color')) {
+          header.classList.add('even-color');
+          meLink.classList.remove('current-link');
+          skillsLink.classList.add('current-link');
+          projectsLink.classList.remove('current-link');
+        }
+      } else {
+        skillsLink.classList.remove('current-link');
+        projectsLink.classList.add('current-link');
+        if (window.innerWidth <= 600) {
+          let footer = document.querySelector('footer'),
+            footerOffset = footer.offsetTop - window.innerHeight + 40;
+          if (window.scrollY < footerOffset) {
+            header.classList.remove('even-color');
+          } else {
+            header.classList.add('even-color');
+          }
+        } else {
+          if (header.classList.contains('even-color')) {
+            header.classList.remove('even-color');
+          }
+        }
+      }
+    }
+  }
+}
+</script>
